9ca1e351dee0eec95e611c2bcd1361759057a6ec
Prayag Sangode <prayag.rhce@gmail.com>
2022-07-01 00:39:48 +0530

    update vars tf code

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_id" {
-    value = aws_s3_bucket.s3-bucket.id
-}
-output "s3_bucket_arn" {
-    value = aws_s3_bucket.s3-bucket.arn
-}
-output "s3_bucket_region" {
-    value = aws_s3_bucket.s3-bucket.region
-}
